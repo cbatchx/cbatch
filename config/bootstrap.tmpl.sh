99fe8ecbd6543c32199776ebdd0fc8e656292ae8
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2016-07-19 18:59:36 +0200

    config and boostrap template supporting µCVM for docker

--- config/bootstrap.tmpl.sh
+++ config/bootstrap.tmpl.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+GROUPADD="/usr/sbin/groupadd -g {{.User.GID}} {{.User.Groupname}}"
+ADDUSER="/usr/sbin/adduser -d {{.User.HomeDir}} -u {{.User.UID}} -g {{.User.GID}} {{.User.Username}}"
+
+
+# µCernVM specifics
+# This uses the busybox version of groupadd and adduser. Because the normal version causes parrot to crash.
+if [ -f "/UCVM/busybox" ]; 
+then
+    GROUPADD="/UCVM/busybox addgroup -g {{.User.GID}} {{.User.Groupname}}"
+    ADDUSER="/UCVM/busybox adduser -D -h {{.User.HomeDir}} -u {{.User.UID}} -G {{.User.Groupname}} {{.User.Username}}"
+fi
+
 # Create a group for the new user.
-/usr/sbin/groupadd -g {{.User.GID}} {{.User.Groupname}}
+eval $GROUPADD
 
 # Create the new user.
-/usr/sbin/adduser -d {{.User.HomeDir}} -u {{.User.UID}} -g {{.User.GID}} {{.User.Username}}
+eval $ADDUSER
 
 # Run the job as the new user.
-su -c 'exec $@' {{.User.Username}} -- "$@"
+/bin/su -c 'exec $@' {{.User.Username}} -- "$@"
