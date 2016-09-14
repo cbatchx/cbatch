e6adc939206563d74e7a876ca4b31f7cac4c6989
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2016-09-14 17:55:02 +0200

    build script for linux

---
+++ scripts/build_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+version=0.0.6
+
+export GOOS=linux 
+export GOARCH=amd64
+
+cd ../ && go build
+
+mkdir -p release
+
+tar -cvf release/cbatch-$version-amd64linux.tar cbatch config/config.toml config/bootstrap.tmpl.sh 
