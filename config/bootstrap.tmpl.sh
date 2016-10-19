fac2ee2a360ae1ef15a7a228b53e1dc021b80c05
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2016-10-19 11:14:13 +0200

    add some testing data and config / bootstrap file working without torque.

--- config/bootstrap.tmpl.sh
+++ config/bootstrap.tmpl.sh
@@ -2,7 +2,7 @@
 
 GROUPADD="/usr/sbin/groupadd -g {{.User.GID}} {{.User.Groupname}}"
 ADDUSER="/usr/sbin/adduser -d {{.User.HomeDir}} -u {{.User.UID}} -g {{.User.GID}} {{.User.Username}}"
-
+SU="/bin/su"
 
 # µCernVM specifics
 # This uses the busybox version of groupadd and adduser. Because the normal version causes parrot to crash.
@@ -10,6 +10,10 @@ if [ -f "/UCVM/busybox" ];
 then
     GROUPADD="/UCVM/busybox addgroup -g {{.User.GID}} {{.User.Groupname}}"
     ADDUSER="/UCVM/busybox adduser -D -h {{.User.HomeDir}} -u {{.User.UID}} -G {{.User.Groupname}} {{.User.Username}}"
+    SU="/UCVM/busybox su"
+    # Bug with CernVM for docker tmp is	not writable
+    /UCVM/busybox chmod 1777 /tmp
+
 fi
 
 # Create a group for the new user.
@@ -19,4 +23,4 @@ eval $GROUPADD
 eval $ADDUSER
 
 # Run the job as the new user.
-/bin/su -s $@ {{.User.Username}} 
+$SU -m -c "$@" {{.User.Username}}
